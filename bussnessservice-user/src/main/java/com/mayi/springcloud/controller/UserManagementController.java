2b40f5cfd8202a21f04738e97af69c4587de1d2d
tianyana <38311201+tianyana@users.noreply.github.com>
2018-06-19 11:16:07 +0800

    Update UserManagementController.java
    被刷新类需要添加@RefreshScope注解

--- bussnessservice-user/src/main/java/com/mayi/springcloud/controller/UserManagementController.java
+++ bussnessservice-user/src/main/java/com/mayi/springcloud/controller/UserManagementController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class UserManagementController {
 	
 	@Value("${server.port}")
